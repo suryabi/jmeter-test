138d26e4a1b2fcb66232dfcc43c21793120af6f5
Pratyush Mishra <pratyush.mishra@briefingiq.com>
2026-08-19 14:32:03 +0530

    Jmeter fixes

--- server.js
+++ server.js
@@ -208,7 +208,7 @@ function writeRunMeta(runDir, meta) {
 
 function setCors(res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 }
 
